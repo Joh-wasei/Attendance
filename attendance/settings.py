211a9337cbd948365e5c78ef63bb038fd99e3f82
joh <j.odagiri.2011@gmail.com>
2022-03-09 11:40:38 +0900

    SQLiteからMySQLに設定変更

--- attendance/settings.py
+++ attendance/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'attendance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'attendance_db',
+        'USER': 'root',
+        'PASSWORD': 'XXXXXXXXX',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
